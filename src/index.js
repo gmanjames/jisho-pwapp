3e7ccc22b353e101085000b1b221feb695d0835b
gmanjames <yms1465@gmail.com>
2019-05-04 17:00:02 -0500

    Add initial hyperHTML test with data

--- src/index.js
+++ src/index.js
@@ -1,4 +1,27 @@
-'use strict';
 import {bind, wire} from 'hyperhtml';
+import jishoData from './data.json';
 
-console.log('reloaded');
+// This is so eslint will only complain once
+const log = console.log.bind(console);
+
+const renderFn = bind(document.getElementById('dataTable'));
+
+const tableHeader = wire()`<thead><tr><th>Kanji</th><th>Pronunciation</th><th>Meaning</th></thead>`;
+function display() {
+    return renderFn`
+        ${tableHeader}
+        <tbody>
+            ${jishoData.words[0]['1-36'].map((word) => {
+                return wire()`
+                    <tr>
+                        <td>${word.kanji}</td>
+                        <td>${word.pronun}</td>
+                        <td>${word.meaning}</td>
+                    </tr>
+                `
+            })}
+        </tbody>
+    `;
+}
+
+display();
